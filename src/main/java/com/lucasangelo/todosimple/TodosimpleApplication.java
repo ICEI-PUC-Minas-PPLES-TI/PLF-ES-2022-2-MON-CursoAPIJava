ab9a862a6c6fa0668bd2a6cbead71161d59a18b0
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-07-30 19:44:38 -0300

    Spring Boot project

---
+++ src/main/java/com/lucasangelo/todosimple/TodosimpleApplication.java
@@ -0,0 +1,13 @@
+package com.lucasangelo.todosimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasangelo/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasangelo.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
