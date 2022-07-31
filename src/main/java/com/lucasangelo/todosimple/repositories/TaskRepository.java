cf220fe835b74d07a6433bda4aa8b7d405fbdcf3
Lucas Angelo O M Rocha <lcs2001_@hotmail.com>
2022-07-31 19:00:05 -0300

    Create User and Task repositories

---
+++ src/main/java/com/lucasangelo/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,22 @@
+package com.lucasangelo.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasangelo.todosimple.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_Id(Long id);
+
+    // @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+    // List<Task> findByUser_Id(@Param("id") Long id);
+
+    // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery =
+    // true)
+    // List<Task> findByUser_Id(@Param("id") Long id);
+
+}
---
+++ src/main/java/com/lucasangelo/todosimple/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.lucasangelo.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasangelo.todosimple.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
